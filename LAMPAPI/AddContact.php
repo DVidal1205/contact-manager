96ea8efc07587d545b85d191a9f3452857e3c79f
ErnestoPerez2801 <147225351+ErnestoPerez2801@users.noreply.github.com>
2025-01-23 18:01:12 -0500

    Testing AddContact again

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,52 +1,48 @@
-
 <?php
 
 $inData = getRequestInfo();
 
-$id = 0;
-$firstName = "";
-$lastName = "";
+$name = "";
+$email = "";
+$phone = "";
 
 $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
 if ($conn->connect_error) {
-	returnWithError($conn->connect_error);
+    returnWithError($conn->connect_error);
 } else {
-	$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-	$stmt->bind_param("ss", $inData["login"], $inData["password"]);
-	$stmt->execute();
-	$result = $stmt->get_result();
+    $stmt = $conn->prepare("INSERT INTO Contacts (Name, Email, Phone) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $inData["name"], $inData["email"], $inData["phone"]);
 
-	if ($row = $result->fetch_assoc()) {
-		returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
-	} else {
-		returnWithError("No Records Found");
-	}
+    if ($stmt->execute()) {
+        returnWithInfo($row['name'], $row['email'], $row['phone']);
+    } else {
+        returnWithError("Failed to add contact: " . $stmt->error);
+    }
 
-	$stmt->close();
-	$conn->close();
+    $stmt->close();
+    $conn->close();
 }
 
 function getRequestInfo()
 {
-	return json_decode(file_get_contents('php://input'), true);
+    return json_decode(file_get_contents('php://input'), true);
 }
 
 function sendResultInfoAsJson($obj)
 {
-	header('Content-type: application/json');
-	echo $obj;
+    header('Content-type: application/json');
+    echo $obj;
 }
 
 function returnWithError($err)
 {
-	$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-	sendResultInfoAsJson($retValue);
+    $retValue = '{"An error occurred!":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
 }
 
-function returnWithInfo($firstName, $lastName, $id)
+function returnWithInfo($name, $email, $phone)
 {
-	$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-	sendResultInfoAsJson($retValue);
+    $retValue = '{"Contact successfully created. It is -> Name":"' . $name . '","Email":"' . $email . '","Phone":"' . $phone . '","error":""}';
+    sendResultInfoAsJson($retValue);
 }
-
-?>
