2a4ea3a09df1e4141b5d00ab64627e6ebc3f21da
Dylan Vidal <dylanvidal1204@gmail.com>
2025-01-26 18:22:12 -0500

    Update RegisterUser.php

--- LAMPAPI/RegisterUser.php
+++ LAMPAPI/RegisterUser.php
@@ -14,14 +14,26 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     returnWithError($conn->connect_error);
 } else {
-    // Insert user into the Users table
     $stmt = $conn->prepare("INSERT into Users(firstName, lastName, Login, Password) VALUES (?,?,?,?)");
-    $stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["login"], $inData["password"]);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    $stmt->bind_param(
+        "ssss",
+        $inData["firstName"],
+        $inData["lastName"],
+        $inData["login"],
+        $inData["password"]
+    );
 
-    if ($row = $result->fetch_assoc()) {
-        returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
+    if (!$stmt->execute()) {
+        returnWithError($stmt->error);
+        $stmt->close();
+        $conn->close();
+        exit();
+    }
+
+    if ($stmt->affected_rows > 0) {
+        $newUserId = $conn->insert_id;
+
+        returnWithInfo($inData["firstName"], $inData["lastName"], $newUserId);
     } else {
         returnWithError("User not Registered");
     }
@@ -52,5 +64,4 @@ function returnWithInfo($firstName, $lastName, $id)
     $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
     sendResultInfoAsJson($retValue);
 }
-
 ?>
