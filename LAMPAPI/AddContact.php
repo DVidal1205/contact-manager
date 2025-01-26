854f4471bcfec5de368fb2ae92db2b3b7ff69f93
Dylan Vidal <dylanvidal1204@gmail.com>
2025-01-26 18:42:22 -0500

    add ID in return

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -20,16 +20,15 @@ if ($conn->connect_error) {
     returnWithError($conn->connect_error);
 } else {
     $stmt = $conn->prepare("INSERT into Contacts(FirstName, LastName, Email, Phone) VALUES (?,?,?,?)");
-    // Bind the parameters correctly
     $stmt->bind_param("ssss", $firstName, $lastName, $email, $phone);
 
-    // Try executing and handle any errors
     if (!$stmt->execute()) {
         returnWithError($stmt->error);
     } else {
+        $newId = $conn->insert_id;
         $stmt->close();
         $conn->close();
-        returnWithInfo("$firstName $lastName", $email, $phone);
+        returnWithInfo($newId, "$firstName $lastName", $email, $phone);
     }
 }
 
@@ -46,12 +45,13 @@ function sendResultInfoAsJson($obj)
 
 function returnWithError($err)
 {
-    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    $retValue = '{"id":0,"firstName":"","lastName":"","email":"","phone":"","error":"' . $err . '"}';
     sendResultInfoAsJson($retValue);
 }
 
-function returnWithInfo($name, $email, $phone)
+function returnWithInfo($id, $name, $email, $phone)
 {
-    $retValue = '{"message":"Contact added.","Name":"' . $name . '","Email":"' . $email . '","Phone":"' . $phone . '","error":""}';
+    $retValue = '{"message":"Contact added.","id":' . $id . ',"Name":"' . $name . '","Email":"' . $email . '","Phone":"' . $phone . '","error":""}';
     sendResultInfoAsJson($retValue);
 }
+?>
